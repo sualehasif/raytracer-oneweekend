b63dfae0da04831c0e4eed52f87dbf9cc7edc510
sualehasif996 <sualehasif996@gmail.com>
2020-12-19 06:27:08 +0500

    removed unnecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sualeh/code/raytracer-oneweekend
-# Build directory: /Users/sualeh/code/raytracer-oneweekend/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
