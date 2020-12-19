1405349c59d96b2410279d92da500db916a183a9
sualehasif996 <sualehasif996@gmail.com>
2020-12-19 06:23:55 +0500

    setting up the codebase for the project.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sualeh/code/raytracer-oneweekend
+# Build directory: /Users/sualeh/code/raytracer-oneweekend/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
